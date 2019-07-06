5341396e851e8017262f3c7d9ead1213fdf31f05
Chris Adams <chris@productscience.co.uk>
2019-07-06 12:23:20 +0200

    Try out tailwind CSS

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
-import router from './router'
+import Vue from "vue"
+import App from "./App.vue"
+import store from "./store"
+import router from "./router"
 
 Vue.config.productionTip = false
+
+import "@/assets/css/tailwind.css"
 
 new Vue({
   store,
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app")
