be555d733ff3d38824932a8c27610e1e6b1933a3
Chris Adams <chris@productscience.co.uk>
2019-07-06 15:16:28 +0200

    Add the extra methods for adding and removing trips

---
+++ src/services/FrequentFlyerLevyService.js
@@ -0,0 +1,13 @@
+const debug = require("debug")("fot:front:frequenFlyerLevyService")
+const fflMultipliers = [0, 0.09, 0.24, 0.46, 0.74, 1.09, 1.49, 1.93, 2.4]
+
+export default {
+  addLevyToFlightCost(cost, pos) {
+    // we have a ceiling, because the trips max out at 9
+    if (pos > 9) {
+      post = 9
+    }
+    const levy = cost * fflMultipliers[pos]
+    return cost + levy
+  }
+}
--- src/store.js
+++ src/store.js
@@ -2,10 +2,13 @@ import Vue from "vue"
 import Vuex from "vuex"
 Vue.use(Vuex)
 import CarbonkitService from "@/services/CarbonkitService.js"
+import FrequentFlyerLevyService from "@/services/FrequentFlyerLevyService.js"
+
 const debug = require("debug")("fot:frontend:store")
 
 export default new Vuex.Store({
   state: {
+    showFFL: false,
     trips: [],
     trip: {},
     totalCo2: 0,
@@ -15,6 +18,14 @@ export default new Vuex.Store({
     totalCO2e: state => {
       let reducer = (accumulator, trip) => accumulator + trip.co2e
       return state.trips.reduce(reducer, 0)
+    },
+    totalDistance: state => {
+      let reducer = (accumulator, trip) => accumulator + trip.distance
+      return state.trips.reduce(reducer, 0)
+    },
+    totalSpent: state => {
+      let reducer = (accumulator, trip) => accumulator + trip.cost
+      return state.trips.reduce(reducer, 0)
     }
   },
   mutations: {
@@ -23,15 +34,17 @@ export default new Vuex.Store({
       state.trips.push(trip)
     },
     DELETE_TRIP(state, tripToRemove) {
-      state.trips.filter(trip => {
+      const newTripList = state.trips.filter(trip => {
         trip.id !== tripToRemove.id
       })
+      state.trips = newTripList
+    },
+    TOGGLE_FFL(state) {
+      state.showFFL = !state.showFFL
     }
   },
   actions: {
-    addTrip({ commit, dispatch }, trip) {
-      debug("ADD TRIP YO")
-      // fetch stuff, init
+    addTrip({ commit }, trip) {
       CarbonkitService.getFlightCalc(trip.from, trip.to)
         .then(response => {
           // populate the trip with distance and CO2
@@ -43,13 +56,19 @@ export default new Vuex.Store({
           })[0]
           trip.co2e = Math.floor(co2e.value)
           trip.distance = Math.floor(distance.value)
-          // console.log(response.data.amounts)
+          trip.cost = Math.round(trip.cost)
           commit("ADD_TRIP", trip)
         })
         // rescue for when it breaks
         .catch(error => {
           debug(`eep. error at`, error)
         })
+    },
+    deleteTrip({ commit }, trip) {
+      commit("DELETE_TRIP", trip)
+    },
+    toggleFFL({ commit }) {
+      commit("TOGGLE_FFL")
     }
   }
 })
