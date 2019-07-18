5698c18bb753c694fd4f4e8aff8c96f5e85a7055
Chris Adams <chris@productscience.co.uk>
2019-07-18 13:32:54 +0200

    Update UI to be a show focus better

--- src/components/TripContainer.vue
+++ src/components/TripContainer.vue
@@ -2,37 +2,48 @@
   <div class>
     <form
       @submit.prevent="createTrip"
-      class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full"
+      class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/2 flex flex-wrap -mx-3 mb-6"
     >
-      <input
-        class="bg-white focus:outline-0 focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 appearance-none leading-normal"
-        v-model="newTrip.from"
-        type="text"
-        placeholder="Starting Airport"
-      />
-      <input
-        class="bg-white focus:outline-0 focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 mr-2 my-2 appearance-none leading-normal"
-        v-model="newTrip.to"
-        type="text"
-        placeholder="Destination Airport"
-      />
-      <input
-        class="bg-white focus:outline-0 focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 mr-2 my-2 appearance-none leading-normal"
-        v-model="newTrip.cost"
-        type="number"
-        placeholder="Approx cost"
-      />
+      <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+        <label class="mr-2" for="starting-airport">Starting Airport</label>
+        <input
+          class="appearance-none bg-gray-200 block borderborder-gray-300 focus:bg-white focus:outline-0 focus:outline-none focus:shadow-outline leading-normal mb-3 px-4 py-3 rounded-lg text-gray-700 w-full"
+          v-model="newTrip.from"
+          type="text"
+          id="starting-airport"
+          placeholder="Starting Airport"
+        />
+      </div>
 
+      <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+        <label class="mr-2 w-1-2" for="destination-airport">Destination Airport</label>
+        <input
+          class="appearance-none bg-gray-200 block borderborder-gray-300 focus:bg-white focus:outline-0 focus:outline-none focus:shadow-outline leading-normal mb-3 px-4 py-3 rounded-lg text-gray-700 w-full"
+          v-model="newTrip.to"
+          type="text"
+          placeholder="Destination Airport"
+        />
+      </div>
+      <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+        <label class="mr-2 w-1-2" for="Approx cost">Rough cost (optional)</label>
+        <input
+          class="appearance-none bg-gray-200 block borderborder-gray-300 focus:bg-white focus:outline-0 focus:outline-none focus:shadow-outline leading-normal mb-3 px-4 py-3 rounded-lg text-gray-700 w-full"
+          v-model="newTrip.cost"
+          type="number"
+          placeholder="Approx cost"
+        />
+      </div>
+
+      <div class="w-full md:w-full px-3 mb-6 md:mb-0">
       <input
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded focus:outline-none focus:shadow-outline"
         type="submit"
         value="Submit"
       />
+      </div>
     </form>
 
     <TripImporter />
-
-    
 
     <div class="flex flex-wrap">
       <div
@@ -44,13 +55,15 @@
         </div>
         <div>
           <span class="text-3xl">{{ distance }}</span>
-        km flown
+          km flown
         </div>
         <div>
           <span class="text-3xl">{{ spent }}</span>
           spent
         </div>
-        
+
+        <span class="text-3xl">{{ FFLLevy }}</span>
+        extra from the Frequent Flyer Levy
       </div>
 
       <div class="w-full md:w-2/3 my-2">
@@ -58,7 +71,7 @@
           <button
             class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             @click="toggleFFL"
-          >Show with FFL</button>
+          >Hide FFL costs</button>
         </div>
         <div v-else>
           <button
